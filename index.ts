39c82fa094c0e9499a60021d87d1ab706e1a9aef
Igor Nepipenko <inepipenko@muellerone.com>
2021-04-24 13:09:15 +0300

    feat: start [ref:no-ref]

---
+++ index.ts
@@ -0,0 +1,49 @@
+Object.defineProperty(window, "MySweetApp", {value: "v1.0.0", writable: true});
+
+function deliveryMethod() {
+    // TODO
+    return "overnight";
+}
+
+function shipWeight() {
+    const el: HTMLElement = document.getElementById('weight') as HTMLDivElement;
+    if (!el) {
+        return 0;
+    }
+    return parseInt(el.innerHTML);
+}
+
+/**
+ * @param {(string | string[])} emailAddr
+ */
+function sendUpdates(emailAddr: string | string[]) {
+    /**
+     * @param {string} addr
+     */
+    function sendEmail(addr: string) {
+        console.log(`Shipping to ${addr} via ${deliveryMethod() || "standard"} delivery`);
+
+        if (shipWeight() > 100) {
+            console.log("WARNING: Oversize package");
+        }
+    }
+
+    if (Array.isArray(emailAddr)) {
+        emailAddr.forEach((val) => {
+            sendEmail(val.trim());
+        });
+    } else {
+        sendEmail(emailAddr.trim());
+    }
+}
+
+// let input = document.querySelector('input') as HTMLInputElement
+// input.addEventListener('input', (e) => {
+//     const el = e.target as HTMLInputElement;
+//     // el.value
+// })
+
+
+// function controller(_: any, res: any) {
+//     return res;
+// }
